2417dc14fb9e7cf9c48b777cad76e462e75e562a
bradleyhh <bradleyhowletth@gmail.com>
2022-08-11 22:48:46 -0700

    wsl.sh echo update

--- play/wsl.sh
+++ play/wsl.sh
@@ -1,4 +1,6 @@
 ifconfig | grep inet | sed -e '2,4d' -e 's/  netmask/\n\t  netmask/' -e 's/broadcast/\n\tbroadcast/' | sed -e '2,3d' -e 's/        inet //' > ./tmp-ip.txt
 ip=$(cat ./tmp-ip.txt)
+echo -e "changing hostname to: ${ip}\n..."
 sed -i "s,window.location.hostname,\"${ip}\"," ./public/js/client.mjs
 rm ./tmp-ip.txt
+head -2 ./public/js/client.mjs
