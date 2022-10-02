f183f5ac6ed72694550480be401e5baa16d2a3e6
Matthew Howlett <mhowlett@michiganaerospace.com>
2022-10-01 23:16:36 -0600

    break game.mjs add chatbox sandbox... mid-dev commit

--- play/startup.sh
+++ play/startup.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
-# WebSocket chat server
-#
-# WebSocket and WebRTC based multi-user chat sample with two-way video
-# calling, including use of TURN if applicable or necessary.
-#
-# This file is the startup script, responsible for installing needed
-# components and starting up the server itself.
-#
-# To read about how this sample works:  http://bit.ly/webrtc-from-chat
-#
-# Any copyright is dedicated to the Public Domain.
-# http://creativecommons.org/publicdomain/zero/1.0/
+# Started this script from the MDN WebSocket chat server example code
 
 npm install websocket
-
+npm install pixi-text-input
+npm install stats
 npm install webrtc-adapter
 cp node_modules/webrtc-adapter/out/adapter.js .
 
