4b3017a8e00bd316dc6903b25893d3f027202ad2
Matthew Howlett <mhowlett@michiganaerospace.com>
2022-10-01 23:23:28 -0600

    game.mjs actually not broken... update wsl dir from public to play

--- play/wsl.sh
+++ play/wsl.sh
@@ -1,6 +1,6 @@
 ifconfig | grep inet | sed -e '2,4d' -e 's/  netmask/\n\t  netmask/' -e 's/broadcast/\n\tbroadcast/' | sed -e '2,3d' -e 's/        inet //' > ./tmp-ip.txt
 ip=$(cat ./tmp-ip.txt)
 echo -e "changing hostname to: ${ip}\n..."
-sed -i "s,window.location.hostname,\"${ip}\"," ./public/js/client.mjs
+sed -i "s,window.location.hostname,\"${ip}\"," ./play/js/client.mjs
 rm ./tmp-ip.txt
-head ./public/js/client.mjs | grep "var myHostname" 
+head ./play/js/client.mjs | grep "var myHostname" 
