ae4c4a8828752ca2889237d7969492e6a8142cc2
Matthew Howlett <matt.r.howlett@gmail.com>
2022-11-05 12:56:03 -0600

    update wsl.sh

--- game/wsl.sh
+++ game/wsl.sh
@@ -1,6 +1,6 @@
 ifconfig | grep inet | sed -e '2,4d' -e 's/  netmask/\n\t  netmask/' -e 's/broadcast/\n\tbroadcast/' | sed -e '2,3d' -e 's/        inet //' > ./tmp-ip.txt
 ip=$(cat ./tmp-ip.txt)
 echo -e "changing hostname to: ${ip}\n..."
-sed -i "s,window.location.hostname,\"${ip}\"," ./game/scripts/libs/client.mjs
+sed -i "s,window.location.hostname,\"${ip}\"," ./scripts/client.mjs
 rm ./tmp-ip.txt
-head ./game/scripts/libs/client.mjs | grep "var myHostname" 
+head ./scripts/client.mjs | grep "var myHostname" 
